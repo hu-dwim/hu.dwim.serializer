f61165f63874f3055c191f4280e33c738caf18c1
levente.meszaros <levente.meszaros@gmail.com>
2010-04-07 13:24:04 +0000

    Uniformize def package

--- source/package.lisp
+++ source/package.lisp
@@ -4,7 +4,7 @@
 ;;;
 ;;; See LICENCE for details.
 
-(in-package :hu.dwim.util)
+(in-package :hu.dwim.def)
 
 (def package :hu.dwim.serializer
   (:use :babel
@@ -14,4 +14,4 @@
         :hu.dwim.util)
   (:shadow #:read-string
            #:write-string)
-  (:readtable-setup (enable-standard-hu.dwim-syntaxes)))
+  (:readtable-setup (hu.dwim.util:enable-standard-hu.dwim-syntaxes)))
